6d8f020c28adad085a1503502fe71a758cd2c50d
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-02-19 23:58:50 +0530

    added shop page layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./components/Homepage/Homepage";
+import ShopPage from "./components/ShopPage/ShopPage";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/ShopPage/ShopPage.jsx
@@ -0,0 +1,16 @@
+import CollectionPreview from "../CollectionPreview/CollectionPreview";
+import SHOP_DATA from "./Shop.data.ts";
+
+function ShopPage() {
+  const shopData = SHOP_DATA;
+
+  return (
+    <div className="shop-page">
+      {shopData.map(({ id, title, items }) => (
+        <CollectionPreview key={id} title={title} items={items} />
+      ))}
+    </div>
+  );
+}
+
+export default ShopPage;
